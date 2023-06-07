21062fcb80d6fc8b8e657e55aa7de70f128aa4dc
Edward Schamp <edwardjls@gmail.com>
2023-06-07 14:04:47 +0100

    gpt-3.5-turbo support

--- frontend/openai_api.py
+++ frontend/openai_api.py
@@ -1,6 +1,7 @@
 from . import views
 import openai
 import os
+import pdb
 
 # OpenAI API key defined in .env file, not shared here
 openai.api_key = os.environ.get('OPENAI_API_KEY')
@@ -18,6 +19,16 @@ def generate_text(prompt):
     )
     return response.choices[0].text.strip()
 
+def generate_better_text(prompt):
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    # pdb.set_trace()
+    return response.choices[0]['message']['content']
+
 activities_dict = {
     "nature" : "Experiencing the local nature \n",
     "wildlife" : "Seeing the local wildlife \n",
@@ -34,6 +45,8 @@ def construct_prompt(dict):
 
     # Output tone and context, done to optimise the answer quality
     prompt += "Pretend that you are a travel agency, specialised in tailoring vacations to the specific interests of each client. \
+            Your job is to provide the best possible suggestions to the client, for them to enjoy their holiday. You do not provide any \
+            services for the client. \n \
             A client has given you the following information in order to construct them a detailled holiday plan. The ideal travel \
             itinerary is tailored to the specific needs of the client, but also includes all must-see tourist attractions in the destination. \n"
     
@@ -56,7 +69,7 @@ def construct_prompt(dict):
     prompt += "***"
 
     # Output formatting - done using prompt engineering
-    prompt += "Give your response in HTML format. \n \
+    prompt += "Give your response in HTML format. The response should be the part that is contained within the <body> tag of the HTML code. \n \
             Each Day of the trip should be contained within it's own border element. These border elements should have 10 pixels of padding, \
             and a border width of 5 pixels, with a light grey shading and a rounded border. \
             There should be 15 pixels of vertical space between border elements. \n \
@@ -70,12 +83,20 @@ def construct_prompt(dict):
         2.5 hours before departure time until departure time for travel back to the airport. \n"
     if dict['timing_preciseness'] == "Specific Times Given":
         prompt += "Provide the specific time range for when each activity should be done. Each activity heading should be in h4 tags, and be in the form \
-            '[activity name] [related emoji] ([start time - end_time])' \
+            '[activity name] ([start time - end_time])' \
             Timings must be specific for every activity. \
             On days when the client is not travelling to or from the airport, the day should be filled from 9:00 to 20:00."
     else:
         prompt += "Provide rough timings for each activity, splitting the activities up into 'morning', 'afternoon' and 'evening' for each day. \
-            Each activity heading should be in h4 tags, and be in the form '[Morning/Afternoon/Evening] - [activity name] [related emoji]'."
+            Each activity heading should be in h4 tags, and be in the form '[Morning/Afternoon/Evening] - [activity name]'. \n"
+    
+    # # Include food suggestions inside of the travel plan
+    # prompt += "Food suggestions are very important to any tourist's travel experience. For each day, in between the suggested activities, give suggestions \
+    #         for meals, with the h4 headings 'Breakfast', 'Lunch', and 'Dinner', followed by a short description of the food, and an address or area to go to to find it. \
+    #         Food suggestions should be close to the area that the client is travelling in. \n"
+    
+    prompt += f"Once again, remember to leave 2.5 hours after the client arrives at {dict['arrival_time']} on {dict['arrival_date']} for travel, and 2.5 \
+            hours before they leave at {dict['departure_time']} on {dict['departure_date']}, also for travel."
     
     return prompt
 
@@ -83,4 +104,4 @@ def construct_prompt(dict):
 def create_travel_itinerary(dict):
     full_prompt = construct_prompt(dict)
     # generate output from engineered prompt
-    return generate_text(full_prompt)
+    return generate_better_text(full_prompt)
