e12df35b4be801f74e068dfaadebc20cf51101c4
Edward Schamp <edwardjls@gmail.com>
2023-06-22 18:12:33 +0100

    added vercel static files route

--- TravelGPT/settings.py
+++ TravelGPT/settings.py
@@ -117,9 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+MEDIA_URL = "img/"
+MEDIA_ROOT = BASE_DIR/"img"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- TravelGPT/urls.py
+++ TravelGPT/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-# import static
+from django.conf import settings
+from django.conf.urls.static import static
 from . import settings
 
 urlpatterns = [
@@ -25,5 +26,5 @@ urlpatterns = [
     path("ariadne/", include("ariadne.urls")),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
