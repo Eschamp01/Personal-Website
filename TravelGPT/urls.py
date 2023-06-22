761de9ac07613d88ddb529eae07db4a70c231fe4
Edward Schamp <edwardjls@gmail.com>
2023-06-22 17:07:09 +0100

    added vercel static files route

--- TravelGPT/urls.py
+++ TravelGPT/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 import static
-import settings
+from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
