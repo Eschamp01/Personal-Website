98a8e31bc4867b92eb6a013700e9b2531383640a
Edward Schamp <edwardjls@gmail.com>
2023-06-22 16:15:18 +0100

    added vercel static files route

--- TravelGPT/urls.py
+++ TravelGPT/urls.py
@@ -24,6 +24,3 @@ urlpatterns = [
     path("travel_plan/", include("frontend.urls")),
     path("ariadne/", include("ariadne.urls")),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
