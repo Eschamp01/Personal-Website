9264005fd981501187d5d249e39ec7a0968e72c6
Edward Schamp <edwardjls@gmail.com>
2023-06-22 18:58:07 +0100

    added vercel static files route

--- build.sh
+++ build.sh
@@ -1,2 +1,23 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+#!/bin/bash
+
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Make migrations - no database used so not needed.
+# echo "Making migrations..."
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
