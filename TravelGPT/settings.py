63c6b772ff87267eef571ce63d25c3a9e74e888f
Edward Schamp <edwardjls@gmail.com>
2023-06-22 18:59:43 +0100

    added vercel static files route

--- TravelGPT/settings.py
+++ TravelGPT/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = "TravelGPT.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+DATABASES = {}
 
 
 # Password validation
