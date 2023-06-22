7f310317bb769d4e00b3825888623b427960af5e
Edward Schamp <edwardjls@gmail.com>
2023-06-22 19:10:41 +0100

    added vercel static files route

--- TravelGPT/settings.py
+++ TravelGPT/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = "django-insecure-xe8#ehh(ttiiak5$uyy5c%y&v&bpw8y5r_m2d(w6c7r=h$1+rh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['travel-gpt-rc73-hk3a1lwe2-eschamp01.vercel.app', 'travel-gpt-rc73.vercel.app', 'travel-gpt-tzrd.vercel.app', \
-                  'travel-gpt-iota.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 
 # Application definition
